de9adbd15cbbad8074be8df2970540b986421dfe
Cal Henderson <cal@iamcal.com>
2010-09-16 00:31:12 -0500

    vector based movement

--- engine.js
+++ engine.js
@@ -12,13 +12,15 @@ exports.gameEngine = function(){
 		var p = new player(client);
 		self.players[p.uid] = p;
 
-		client.broadcast({msg: 'joined', uid: p.uid, player: p.get_state()});
+		client.broadcast({msg: 'joined', uid: p.uid, player: { state: p.get_state()}});
 
 		// send a list of connected players
 		var msg = {msg: 'welcome', players: {}};
 		for (var i in self.players){
 			var p2 = self.players[i];
-			msg.players[i] = p2.get_state();
+			msg.players[i] = {
+				state: p2.get_state(),
+			};
 			if (i == p.uid){
 				msg.players[i].self = 1;
 			}
@@ -47,7 +49,9 @@ exports.gameEngine = function(){
 	};
 }
 
-
+function random(lo, hi){
+	return lo + Math.floor(Math.random()*(1+hi-lo));
+}
 
 function player(client){
 
@@ -55,22 +59,67 @@ function player(client){
 
 	this.client = client;
 	this.uid = client.sessionId;
-	this.x = 200;
-	this.y = 200;
+	this.x = random(20,400);
+	this.y = random(20,100);
+	this.moveSpeed = 50; // pixels per second
 
 	this.get_state = function(){
+		self.update_move();
 		return {
 			x: self.x,
 			y: self.y,
+			v: self.vector,
 		};
 	};
 
 	this.move = function(x, y){
 
-		this.x = x;
-		this.y = y;
-		client.broadcast({msg: 'moved', uid: this.uid, x:x, y:y});
-		client.send({msg: 'moved', uid: this.uid, x:x, y:y});
+		if (x == self.x && y == self.y) return;
+
+		// stop any pending movement vector first
+		self.stop_move();
+
+		// how far are they going and how long will it take
+		var dx = Math.abs(self.x - x);
+		var dy = Math.abs(self.y - y);
+		var dist = Math.sqrt(dx * dx + dy * dy);
+		var time = (dist / self.moveSpeed) * 1000;
+
+		// create the new vector
+		self.vector = {
+			from: [self.x, self.y],
+			to: [x, y],
+			start: new Date().getTime(),
+			len: time,
+		};
+
+		var state = self.get_state();
+		client.send({msg: 'move', uid: self.uid, state: state});
+		client.broadcast({msg: 'move', uid: self.uid, state: state});
+		// boradcast this too!
+	};
+
+	this.stop_move = function(){
+
+		self.update_move();
+		delete self.vector;
+	};
+
+	this.update_move = function(){
+
+		if (!self.vector) return;
+
+		var now = new Date().getTime();
+		var frac = (now - self.vector.start) / self.vector.len;
+
+		if (frac >= 1){
+			self.x = self.vector.to[0];
+			self.y = self.vector.to[1];
+			delete self.vector;
+		}else{
+			self.x = self.vector.from[0] + frac * (self.vector.to[0] - self.vector.from[0]);
+			self.y = self.vector.from[1] + frac * (self.vector.to[1] - self.vector.from[1]);
+		}
 	};
 }
 
