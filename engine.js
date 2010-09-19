5c55ff78d9157257337c8aefbb4934bc415ac301
Myles Grant <myles@mylesgrant.com>
2010-09-20 02:19:00 +0800

    Adding comments, small lint fix

--- engine.js
+++ engine.js
@@ -5,13 +5,19 @@ exports.gameEngine = function(){
 
 	this.players = {};
 
+	//
+	// add a newly-connected player to the world
+	//
+	
 	this.addPlayer = function(client){
 
 		console.log('adding a player');
 
+		// create a new player object from the client
 		var p = new player(client);
 		self.players[p.uid] = p;
 
+		// tell all the other players about it
 		client.broadcast({msg: 'joined', uid: p.uid, player: { state: p.get_state()}});
 
 		// send a list of connected players
@@ -28,41 +34,64 @@ exports.gameEngine = function(){
 		client.send(msg);
 	};
 
+	//
+	// remove a player from the world (disconnect, etc)
+	//
+	
 	this.removePlayer = function(client){
 		var uid = client.sessionId;
 		delete self.players[uid];
 
+		// tell all the other players about it
 		client.broadcast({msg: 'left', uid: uid});
 	};
 
+	//
+	// handle player commands from the client
+	//
+	
 	this.playerMsg = function(client, msg){
 
 		var uid = client.sessionId;
 		var p = self.players[uid];
 
-		if (msg.msg == 'move'){
-			p.move(msg.x, msg.y);
-			return;
+		if (p){
+			// move to x,y
+			if (msg.msg == 'move'){
+				p.move(msg.x, msg.y);
+				return;
+			}
 		}
 
 		client.send({msg: 'unhandled', src: msg});
 	};
-}
+};
 
 function random(lo, hi){
 	return lo + Math.floor(Math.random()*(1+hi-lo));
 }
 
+
+//
+// a player object
+//
+
 function player(client){
 
 	var self = this;
 
-	this.client = client;
+	this.client = client; // reference to their socket.io client
 	this.uid = client.sessionId;
-	this.x = random(20,400);
+	this.x = random(20,400); // put them in a random position
 	this.y = random(20,100);
 	this.moveSpeed = 50; // pixels per second
 
+
+	//
+	// get compact state information about a player
+	// includes current position and movement info
+	//
+	
 	this.get_state = function(){
 		self.update_move();
 		return {
@@ -71,6 +100,12 @@ function player(client){
 			v: self.vector,
 		};
 	};
+	
+	
+	//
+	// start a move, calculating path to target and move duration
+	// notifies other players about the move so they can render the steps
+	//
 
 	this.move = function(x, y){
 
@@ -96,14 +131,24 @@ function player(client){
 		var state = self.get_state();
 		client.send({msg: 'move', uid: self.uid, state: state});
 		client.broadcast({msg: 'move', uid: self.uid, state: state});
-		// boradcast this too!
+		// broadcast this too!
 	};
+	
+	
+	//
+	// stop an in-progress move
+	//
 
 	this.stop_move = function(){
 
 		self.update_move();
 		delete self.vector;
 	};
+	
+	
+	//
+	// update the player's position based on any in-progress moves
+	//
 
 	this.update_move = function(){
 
@@ -112,11 +157,13 @@ function player(client){
 		var now = new Date().getTime();
 		var frac = (now - self.vector.start) / self.vector.len;
 
+		// done?
 		if (frac >= 1){
 			self.x = self.vector.to[0];
 			self.y = self.vector.to[1];
 			delete self.vector;
 		}else{
+			// interpolate
 			self.x = self.vector.from[0] + frac * (self.vector.to[0] - self.vector.from[0]);
 			self.y = self.vector.from[1] + frac * (self.vector.to[1] - self.vector.from[1]);
 		}
